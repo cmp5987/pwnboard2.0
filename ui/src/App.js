c697194993714c4f71a0c574b93c9e63e4275092
Squidli <cmp5987@rit.edu>
2023-01-09 21:22:26 -0500

    changing packages

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import {
-  QueryClient,
-  QueryClientProvider
-} from "react-query";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from './pages/About';
 import HostAccess from './pages/HostAccess';
 
 function App() {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/" element={<HostAccess />} />
         <Route path="about" element={<About />} />
       </Routes>
-    </QueryClientProvider>
   );
 }
 
